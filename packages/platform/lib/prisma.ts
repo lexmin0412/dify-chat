b1c99888f13f78a2b632cf9e72434a3797b32bb8
lexmin0412 <zhangle_dev@outlook.com>
2025-11-27 12:55:30 +0800

    feat: 尝试升级 prisma v7(目前遇到 prisma/generated/client.ts 中引用的文件为 js, 但实际生成的文件是 ts 的问题

--- packages/platform/lib/prisma.ts
+++ packages/platform/lib/prisma.ts
@@ -1,13 +1,37 @@
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+import 'dotenv/config'
+
 import { PrismaClient } from '@/prisma/generated/client'
 
 const globalForPrisma = globalThis as unknown as {
 	prisma: PrismaClient | undefined
 }
 
+const connectionString = process.env.DATABASE_URL
+
+const parseMySqlOptionsFromConnectionString = (connectionString: string) => {
+	const url = new URL(connectionString)
+	return {
+		host: url.hostname,
+		port: parseInt(url.port),
+		user: url.username,
+		password: url.password,
+	}
+}
+const mysqlOptions = parseMySqlOptionsFromConnectionString(connectionString as string)
+
+const adapter = new PrismaMariaDb({
+	host: mysqlOptions.host,
+	port: mysqlOptions.port,
+	user: mysqlOptions.user,
+	password: mysqlOptions.password,
+	connectTimeout: 10000,
+})
+
 export const prisma =
 	globalForPrisma.prisma ??
 	new PrismaClient({
-		log: ['query'],
+		adapter,
 	})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
