1a315ddad723c8ac4ead91e9260fba71ebe38917
lexmin0412 <zhangle_dev@outlook.com>
2026-01-07 23:41:18 +0800

    chore: 升级 Prisma 至 v7，修改配置及使用方式
    - 调整  prisma 实例初始化及配置方式，适应 v7 新 API
    - prisma 相关依赖的版本声明从 workspace catalog 移动至 platform 内部显式声明，降低不必要的维护成本
    - 引入 dotenv，升级 typescript 版本，提升 node 版本要求，以适配 prisma v7 要求

---
+++ packages/platform/prisma.config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
+
+export default defineConfig({
+	schema: 'prisma/schema.prisma',
+	migrations: {
+		path: 'prisma/migrations',
+		seed: 'tsx prisma/seed.ts',
+	},
+	// 这里必须单独定义，用于 Prisma CLI 命令执行
+	datasource: {
+		url: env('DATABASE_URL'),
+	},
+})
