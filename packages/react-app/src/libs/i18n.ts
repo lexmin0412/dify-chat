27f6544f66efb36a49039f769ed55842960116c5
lexmin0412 <zhangle_dev@outlook.com>
2026-01-11 15:28:01 +0800

    optimize(i18n): 在 i18n 的初始化函数中前加上 await，防止配置未初始化完成就加载导致语言配置不生效

--- packages/react-app/src/libs/i18n.ts
+++ packages/react-app/src/libs/i18n.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-i18n
+await i18n
 	// detect user language
 	// learn more: https://github.com/i18next/i18next-browser-languageDetector
 	.use(LanguageDetector)
