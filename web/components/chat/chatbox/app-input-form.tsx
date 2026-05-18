1551e9db02b64ac0f0df3f77e10cb118508f3b17
lexmin0412 <zhangle_dev@outlook.com>
2026-05-18 11:05:37 +0800

    fix: upload_file_id 注释位置修正

--- web/components/chat/chatbox/app-input-form.tsx
+++ web/components/chat/chatbox/app-input-form.tsx
@@ -62,7 +62,8 @@ function normalizeFieldValue(type: IUserInputFormItemType, value: unknown): unkn
 		name: (file as IUploadFileItem).name || (file as IDifyConversationInputFile).filename,
 		url: (file as IUploadFileItem).url || (file as IDifyConversationInputFile).remote_url,
 		status: (file as IUploadFileItem).status || 'done',
-// @ts-expect-error FIXME: 类型待优化 		upload_file_id:
+		// @ts-expect-error FIXME: 类型待优化
+		upload_file_id:
 			(file as IUploadFileItem).upload_file_id || (file as IDifyConversationInputFile).related_id,
 	})
 	if (type === 'file-list' && Array.isArray(value)) {
