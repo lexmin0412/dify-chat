71cdc869914fb5e99aa205487be34ccbcaa3110d
lexmin0412 <zhangle_dev@outlook.com>
2026-05-18 11:41:53 +0800

    fix: config DEBUG_MODE 类型改为 string

--- web/config/index.ts
+++ web/config/index.ts
@@ -1,7 +1,7 @@
 const config = {
 	PUBLIC_APP_API_BASE: '/api/client',
 	PUBLIC_DIFY_PROXY_API_BASE: '/api/client/dify',
-	PUBLIC_DEBUG_MODE: false,
+	PUBLIC_DEBUG_MODE: 'false',
 }
 
 export default config
